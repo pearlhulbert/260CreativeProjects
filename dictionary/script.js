7e30f6206c70dec5d65f50c0106f146992f96e1e
Pearl Hulbert <pearl.k.hulbert@gmail.com>
2022-10-11 23:19:22 +0000

    javascript does some stuff

--- dictionary/script.js
+++ dictionary/script.js
@@ -6,23 +6,22 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
     return;
   console.log(value);
   const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + value;
+  console.log(url);
   fetch(url)
     .then(function(response) {
       return response.json();
     }).then(function(json) {	
       let results = "";
       results += "<h2> Word: " + json.word + "</h2>";
-      results += '<h2>' + json.phonetic + "</h2>"
-      results += '<p>' + json.origin + "</p>"
-      results += "<p>"
-      for (let i=0; i < json.meaning.length; i++) {
+      results += '<h2>' + json.phonetic + "</h2>";
+      results += '<p>' + json.origin + "</p>";
+      console.log(results);
+      /*for (let i=0; i < json.meanings.length; i++) {
     	results += json.meanings[i].description
     	if (i !== json.meanings.length - 1)
     	  results += ", "
     	  results += '<p>' + json.meanings.definitions.definition + "</p>"
-          }
-          results += "</p>";
-          results += "</div>";
+          }*/
           document.getElementById("weatherResults").innerHTML = results;
       });
 });
